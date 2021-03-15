a977ef41aba0815b3519e0502e31bdb918672dbe
Martín Madridejos <martindejos@gmail.com>
2021-03-15 15:21:22 +0100

    switch con JS

--- assets/js/switch.js
+++ assets/js/switch.js
@@ -9,7 +9,9 @@ let paragrafo = document.querySelector (".paragrafo-planta");
 let camaraIcono = document.querySelector (".camara-icon");
 let corazonIcono = document.querySelector (".corazon-icon");
 let circuloBlanco = document.querySelector (".contenedor-img");
-
+let tamañoPlanta = document.querySelector (".planta");
+let fotoPequena = document.getElementsByClassName ("foto-pequena");
+let volverAtrasIcono = document.querySelector (".icon-volveratras");
 
 imgPlanta.addEventListener("click", function() {
    
@@ -31,7 +33,7 @@ imgPlanta.addEventListener("click", function() {
     plantaTitulo.style.fontWeight="700";
 
     paragrafo.style.textAlign="left";
-    paragrafo.style.paddingRight="30%";
+    paragrafo.style.paddingRight="23%";
 
     camaraIcono.style.top="-16%";
     camaraIcono.style.right="25%";
@@ -41,11 +43,75 @@ imgPlanta.addEventListener("click", function() {
 
     circuloBlanco.style.backgroundColor="white";
     circuloBlanco.style.borderRadius= "100%";
+    circuloBlanco.style.height="55vw";
+    circuloBlanco.style.width="55vw";
+    circuloBlanco.style.marginBottom="20px";
+
+    tamañoPlanta.style.width="120%";
+    tamañoPlanta.style.right="15%";
+
+    volverAtrasIcono.style.display="block";
+
+    for (i=0; i < fotoPequena.length; i++){
+    fotoPequena[i].classList.remove("hidden-photo");
+      
+    }
 
 
-    setInterval(function(){ circulo.style.top="-64.5%"}, 200);
+
+    setInterval(function(){ circulo.style.top="-65.5%"}, 200);
 
 });
 
 
+volverAtrasIcono.addEventListener("click", function() {
+   
+    for (i=0; i < hideElements.length; i++){
+        hideElements[i].style.display="";
+    }
     
+    
+    plantaSubtitulo.style.color="";
+    imgPlanta.style.width="";
+    
+    infoContenedor.style.flexDirection=""; 
+    infoContenedor.style.justifyContent="";
+    
+    divTitulo.style.alignItems="";
+    
+    plantaSubtitulo.style.fontSize="";
+    plantaSubtitulo.style.color="";
+    
+    plantaTitulo.style.fontSize="";
+    plantaTitulo.style.fontWeight="";
+
+    paragrafo.style.textAlign="";
+    paragrafo.style.paddingRight="";
+
+    camaraIcono.style.top="";
+    camaraIcono.style.right="";
+
+    corazonIcono.style.top="";
+    corazonIcono.style.left="";
+
+    circuloBlanco.style.backgroundColor="";
+    circuloBlanco.style.borderRadius= "";
+    circuloBlanco.style.height="";
+    circuloBlanco.style.width="";
+    circuloBlanco.style.marginBottom="";
+
+    tamañoPlanta.style.width="";
+    tamañoPlanta.style.right="";
+
+    volverAtrasIcono.style.display="";
+
+    for (i=0; i < fotoPequena.length; i++){
+        fotoPequena[i].classList.add("hidden-photo");
+          
+        }
+    
+    
+    
+        setInterval(function(){ circulo.style.top=""}, 0);
+    
+})
